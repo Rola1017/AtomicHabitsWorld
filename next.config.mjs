ef5d16455f8497f0e44a3a019834abca7e2714ac
Rola1017 <GitHubemail>
2026-04-09 11:01:30 +0800

    add cms subdomain to remotePatterns

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,10 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
+        hostname: "cms.atomichabitsworld.com",
+      },
+      {
+        protocol: "https",
         hostname: "atomichabitsworld.com",
       },
       {
